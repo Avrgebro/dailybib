2973f836299b6e8ed0b6f4563911dcd56c288445
Jose Bejarano <jabcarranz@gmail.com>
2021-02-19 23:14:02 -0500

    [bottomnavbar] Implemented bottom navigation bar

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:dailybib/view/read/index.dart';
+import 'package:dailybib/view/plan/index.dart';
+import 'package:dailybib/view/more/index.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,10 +9,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    ReadPage(),
+    PlanPage(),
+    MorePage()
+  ];
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+        child: _pages[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article),
+            label: 'Lee'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Planea'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.segment),
+            label: 'Más'
+          )
+        ],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
